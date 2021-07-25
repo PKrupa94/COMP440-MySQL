5a8ea1f3d54da07b33829b4a60901b1eff91b984
mzahed603 <mohammad.zahed.603@my.csun.edu>
2021-07-25 12:35:37 -0700

    Minor improvements

--- Server/api/Register.php
+++ Server/api/Register.php
@@ -3,7 +3,6 @@
   header( "Access-Control-Allow-Origin: *" );
   header( "Access-Control-Allow-Headers: access" );
   header( "Access-Control-Allow-Methods: POST" );
-  header( "Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" );
   header( "Content-Type: application/json; charset=UTF-8" );
 
   require __DIR__."/../classes/Database.php";
